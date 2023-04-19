9bde12e487163ace3a88a34d41b1ee8602e91500
deepanprabhu <deepan18@gmail.com>
2023-04-19 14:32:18 +0530

    Classify - new api works

---
+++ src/main/java/classify/Classify.java
@@ -0,0 +1,41 @@
+package classify;
+
+import client.Client;
+import embed.EmbedRequest;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Classify {
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String api = "classify";
+    public static final String BEARER_TOKEN = "Bearer";
+
+    public static void main(String[] args) throws Exception {
+        ClassifyRequest.Example example1 = new ClassifyRequest.Example();
+        example1.setLabel("Spam");
+        example1.setText("Dermatologists don't like her!");
+
+        ClassifyRequest.Example example2 = new ClassifyRequest.Example();
+        example2.setText("Weekly sync notes");
+        example2.setLabel("Not Spam");
+
+        ClassifyRequest.Example example3 = new ClassifyRequest.Example();
+        example3.setLabel("Spam");
+        example3.setText("Please help me?");
+
+        ClassifyRequest.Example example4 = new ClassifyRequest.Example();
+        example4.setText("Your parcel will be delivered today");
+        example4.setLabel("Not Spam");
+
+        ClassifyRequest classifyRequest = ClassifyRequest.builder()
+                .inputs(new HashSet<>(Arrays.asList("Confirm your email address", "hey i need u to send some $")))
+                .examples(new HashSet<>(Arrays.asList(example1, example2, example3, example4)))
+                .truncate("END")
+                .build();
+
+        Client.hit(api, BEARER_TOKEN, classifyRequest, (str)-> {
+            System.out.println(str);
+        });
+    }
+}
--- src/main/java/classify/ClassifyRequest.java
+++ src/main/java/classify/ClassifyRequest.java
@@ -4,11 +4,13 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import util.AbstractRequest;
 
 import java.util.Set;
 
 @Getter
+@Setter
 @Builder
 public class ClassifyRequest extends AbstractRequest {
     private Set<String> inputs;
@@ -17,6 +19,7 @@ public class ClassifyRequest extends AbstractRequest {
     private String truncate;
     private String preset;
 
+    @Setter
     public static class Example {
         private String text;
         private String label;
